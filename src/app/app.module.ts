be2d7f1fcb94df65a9d4fff4e4f0bc05b4f229b9
olivierpoupier <olivier@poupier.net>
2018-11-17 21:19:01 -0500

    fix cropping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+
 import { RouteReuseStrategy } from '@angular/router';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+// Components
 import { AppComponent } from './app.component';
+
+// Modules
 import { AppRoutingModule } from './app-routing.module';
+// import { AngularCropperjsModule } from 'angular-cropperjs';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CameraPageModule } from './camera/camera.module';
+
 import { Camera } from '@ionic-native/camera/ngx';
 import { CameraPreview } from '@ionic-native/camera-preview/ngx';
 import { NativeStorage } from '@ionic-native/native-storage/ngx';
-import { AngularCropperjsModule } from 'angular-cropperjs';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,7 +25,8 @@ import { AngularCropperjsModule } from 'angular-cropperjs';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    AngularCropperjsModule
+    // AngularCropperjsModule,
+    CameraPageModule
   ],
   providers: [
     StatusBar,
